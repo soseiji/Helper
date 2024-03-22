eceac2ee5750b2e1edf6c477197808655417e7f0
Seiji <seiji1111@gmail.com>
2024-03-23 08:42:39 +0900

    base64、gzip解凍機能の追加

---
+++ JsonHelper/JsonHelper/Models/JsonService.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonHelper.Models
+{
+    public class JsonService
+    {
+        public (bool, string) IsValidJson(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return (false, string.Empty);
+            try
+            {
+                // Json形式でない場合、シリアライザーが例外を返す
+                JsonConvert.DeserializeObject<JObject>(text);
+                return (true, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+        public string AddIndent(string text)
+        {
+            // 文字列をそのままシリアライズすると、escapeが入るだけ。文字列を一旦オブジェクトに戻す。
+            //TextBoxOutput.Text = JsonConvert.SerializeObject(input, Formatting.Indented);
+            var jObject = JsonConvert.DeserializeObject<JObject>(text);
+            return JsonConvert.SerializeObject(jObject, Formatting.Indented);
+        }
+
+        public string DelIndent(string text)
+        {
+            var jObject = JsonConvert.DeserializeObject<JObject>(text);
+            return JsonConvert.SerializeObject(jObject, Formatting.None);
+        }
+
+        public string AddEscapeStr(string text)
+        {
+            // シリアライズすれば、ダブルクォーテーションにエスケープ処理がかかる
+            return JsonConvert.SerializeObject(text, Formatting.None);
+        }
+
+        public string DelEscapeStr(string text)
+        {
+            // エスケープされたJson文字列をオブジェクト化し、文字列で出力する
+            var jToken = JsonConvert.DeserializeObject<JToken>(text);
+            return jToken.ToString();
+        }
+
+        public string ConvertFromGzipHexStr(string text)
+        {
+            // 以下の処理を適用した文字列をJsonに変換する。
+            // Json => Gzip => 16進数バイナリ文字列
+            var byteList = new List<byte>();
+            text = text.Replace("0x", "");
+            for (int index = 0; index < text.Length / 2; index++)
+            {
+                byteList.Add(Convert.ToByte(text.Substring(startIndex: index * 2, length: 2), fromBase: 16));
+            }
+            return Encoding.UTF8.GetString(DecompressGZip(byteList.ToArray()));
+        }
+
+        public string ConvertFromGzipBase64(string text)
+        {
+            // 以下の処理を適用した文字列をJsonに変換する。
+            // Json => Gzip => Base64           
+            text = text.Replace(",", "").Replace(" ", "").Replace("　", "");  // Base64変換に不要な文字列を除外
+            var decode = Convert.FromBase64String(text);
+            return Encoding.UTF8.GetString(DecompressGZip(decode));
+        }
+
+        private byte[] DecompressGZip(byte[] data)
+        {
+            MemoryStream inputStream = null;
+            MemoryStream outputStream = null;
+            try
+            {
+                inputStream = new MemoryStream(data);
+                outputStream = new MemoryStream();
+                using (GZipStream gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                {
+                    byte[] buffer = new byte[1024];
+                    while (true)
+                    {
+                        int readSise = gzipStream.Read(buffer, 0, buffer.Length);
+                        if (readSise == 0) break;
+                        outputStream.Write(buffer, 0, readSise);
+                    }
+                }
+                return outputStream.ToArray();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (outputStream != null) outputStream.Dispose();
+                if (inputStream != null) inputStream.Dispose();
+            }
+        }
+    }
+}
---
+++ JsonHelper/JsonHelper/Views/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using JsonHelper.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JsonHelper.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCheck_Click(object sender, RoutedEventArgs e)
+        {
+            (var isSuccess, var errMessage) = jsonService.IsValidJson(TextBoxInput.Text);
+            TextBlockCheck.Text = isSuccess ? "Success !!!" : errMessage;
+        }
+
+        private void ButtonAddIndent_Click(object sender, RoutedEventArgs e) => RunAction(input => TextBoxOutput.Text = jsonService.AddIndent(input));
+        private void ButtonDelIndent_Click(object sender, RoutedEventArgs e) => RunAction(input => TextBoxOutput.Text = jsonService.DelIndent(input));
+        private void ButtonAddEscape_Click(object sender, RoutedEventArgs e) => RunAction(input => TextBoxOutput.Text = jsonService.AddEscapeStr(input));
+        private void ButtonDelEscape_Click(object sender, RoutedEventArgs e) => RunAction(input => TextBoxOutput.Text = jsonService.DelEscapeStr(input));
+        private void ButtonConvertGzipHexStr_Click(object sender, RoutedEventArgs e) => RunAction(input => TextBoxOutput.Text = jsonService.ConvertFromGzipHexStr(input));
+        private void ButtonConvertGzipBase64_Click(object sender, RoutedEventArgs e) => RunAction(input => TextBoxOutput.Text = jsonService.ConvertFromGzipBase64(input));
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e) => (TextBoxInput.Text, TextBoxOutput.Text) = ("", "");
+        private void ButtonSwap_Click(object sender, RoutedEventArgs e) => (TextBoxInput.Text, TextBoxOutput.Text) = (TextBoxOutput.Text, TextBoxInput.Text);
+        private void ButtonInputCopy_Click(object sender, RoutedEventArgs e) => SetClipboard(TextBoxInput.Text);
+        private void ButtonOutputCopy_Click(object sender, RoutedEventArgs e) => SetClipboard(TextBoxOutput.Text);
+
+        // ---------------------------------------------------------------------------------------------
+
+
+
+        private void RunAction(Action<string> action)
+        {
+            var input = TextBoxInput.Text;
+            if (!IsValid(input)) return;
+            try
+            {
+                action.Invoke(input);
+            }
+            catch (Exception ex)
+            {
+                ShowWarnMessage("変換に失敗しました" + Environment.NewLine + ex.Message);
+            }
+        }
+
+        private bool IsValid(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                ShowWarnMessage("入力データがありません");
+                return false;
+            }
+            return true;
+        }
+
+        private void ShowWarnMessage(string msg)
+        {
+            MessageBox.Show(msg, "JsonHelper", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        private void SetClipboard(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return;
+            Clipboard.SetText(text);
+        }
+
+        private readonly JsonService jsonService = new();
+    }
+}
